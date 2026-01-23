142d01aaa1a4ca1f46895c10745052877f2a4798
GoutouStdio-cn <goutougroup8888@outlook.com>
2026-01-23 19:12:23 +0800

    recovery新增optimize命令，此命令可以触发gc垃圾回收

--- recovery.py
+++ recovery.py
@@ -11,6 +11,7 @@ import kernel
 import logk
 import shutil
 import os
+import gc
 
 # 主程序
 def recovery_main(jumpinfo) -> str:
@@ -18,11 +19,12 @@ def recovery_main(jumpinfo) -> str:
     logk.printl("recovery", f"跳入到recovery, jumpinfo={jumpinfo}", main.boot_time)
     logk.printl("recovery", "欢迎使用PySpOS Recovery，输入help获取可用命令", main.boot_time)
     while 1:
-        prompt = input("recovery> ")
+        prompt = input("recovery> ")    # 我们recovery的提示符
 
         if prompt == "help":
             print("help     打印本帮助信息")
             print("erase    清除bootcfg和pycache信息")
+            print("optimize 清理Python运行内存，让性能更高（实验性的）")
             print("exit     退出Recovery\n")
         elif prompt == "erase":
             if os.path.isdir("etc"):
@@ -37,6 +39,11 @@ def recovery_main(jumpinfo) -> str:
             logk.printl("recovery", "清除完成，请重启程序\n", main.boot_time)
         elif prompt == "exit":
             break
+        elif prompt == "optimize":
+            # 触发垃圾回收2次
+            gc.collect()
+            gc.collect(2)
+            logk.printl("recovery", "已清理垃圾！\n", main.boot_time)
         else:
             print(f"找不到 {prompt} 命令")
     
