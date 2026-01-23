87bba63f091daada0289d80017a7a0484b8c8749
GoutouStdio-cn <goutougroup8888@outlook.com>
2026-01-23 10:32:20 +0800

    重新提交，新增Recovery和日志处理，更新README，并把清屏逻辑封装成一个函数

---
+++ recovery.py
@@ -0,0 +1,45 @@
+#
+#   recovery.py
+#   简易的模拟恢复模式
+#
+#   2026/1/23 By GoutouStdio
+#   @2022~2026 GoutouStdio. Open all rights.
+
+import btcfg
+import main
+import kernel
+import logk
+import shutil
+import os
+
+# 主程序
+def recovery_main(jumpinfo) -> str:
+    kernel.screen_clear() # 清屏
+    logk.printl("recovery", f"跳入到recovery, jumpinfo={jumpinfo}", main.boot_time)
+    logk.printl("recovery", "欢迎使用PySpOS Recovery，输入help获取可用命令", main.boot_time)
+    while 1:
+        prompt = input("recovery> ")
+
+        if prompt == "help":
+            print("help     打印本帮助信息")
+            print("erase    清除bootcfg和pycache信息")
+            print("exit     退出Recovery\n")
+        elif prompt == "erase":
+            if os.path.isdir("etc"):
+                shutil.rmtree("etc")
+            else:
+                logk.printl("recovery", "etc文件夹已经清除了", main.boot_time)
+            
+            if os.path.isdir("__pycache__"):
+                shutil.rmtree("__pycache__")
+            else:
+                logk.printl("recovery", "pycache文件夹已经清除了", main.boot_time)
+            logk.printl("recovery", "清除完成，请重启程序\n", main.boot_time)
+        elif prompt == "exit":
+            break
+        else:
+            print(f"找不到 {prompt} 命令")
+    
+    logk.printl("recovery", "Recovery已退出", main.boot_time)
+    
+
