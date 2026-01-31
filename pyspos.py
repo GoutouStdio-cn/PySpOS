f43cbd3c307a46fe68ecbd4a582bafdda0128cf5
GoutouStdio-cn <goutougroup8888@outlook.com>
2026-01-31 21:40:32 +0800

    更新pyspos.c，画了一堆饼并定义 NULL 值

--- pyspos.py
+++ pyspos.py
@@ -6,8 +6,10 @@
 #   @2022~2026 GoutouStdio. Open all rights.
 
 #   2026/1/31 update log: add this file to store base information of PySpOS!!!
-#   ...
+#   2026/1/31 update 2 log: add null value definition, add spui,sunglass definitions
 
+# Null value.
+NULL = 0
 
 # OS Name.
 OS_NAME = "PySpOS"
@@ -36,7 +38,7 @@ OS_VENDOR = "GoutouStdio"
 OS_COPYRIGHT = "@2022~2026 GoutouStdio. Open all rights."
 
 # Select whether to enable spf support.
-SPF_ENABLED = True
+SPF_ENABLED = True 
 
 # SPF parser version.
 SPF_VERSION = "0.1" # The spf parser is too simple, so we think the version should be 0.1.
@@ -45,7 +47,25 @@ SPF_VERSION = "0.1" # The spf parser is too simple, so we think the version shou
 SPC_ENABLED = False # SpaceConfig is disabled for now, because it is not developed yet.
 
 # SpaceConfig Version.
-SPC_VERSION = 0 # SpaceConfig is not developed yet, so we think its version is 0
+SPC_VERSION = NULL # SpaceConfig is not developed yet, so we think its version is 0
+
+# Select whether to enable Space User Interface.
+SPUI_ENABLED = False # SUI2 is disabled for now, because it is not developed yet.
+
+# SPUI Version.
+SPUI_VERSION = NULL # SPUI is not developed yet, so we think its version is 0
+
+
+if SPUI_ENABLED:
+    # Select whether to enable SunGlass.
+    SUNGALASS_ENABLED = True # SunGlass is true by default.
+
+    # SunGlass Version.
+    SUNGALASS_VERSION = NULL # SunGlass is not developed yet, so we think its version is 0
+else:
+    # You don't need these values, because you have disabled SPUI.
+    SUNGALASS_ENABLED = NULL
+    SUNGALASS_VERSION = NULL
 
 # Select whether to enable developer mode.
 DEVELOPER_MODE = False # Developer mode is disabled by default. You can enable it for development and testing purposes.
